e9dcf534a39e7e80bf596701aca5c47c33a2cfb8
Alireza Gharib <alirezagharib110@gmail.com>
2024-08-07 12:42:52 +0330

    Update setup_index.sh

--- ahmia-index/setup_index.sh
+++ ahmia-index/setup_index.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# If the environment file does not exist, create it based on example.env
-if [ ! -f .env ]; then
-    echo "Warning: File .env not found, copying from example.env"
-    cp example.env .env
-fi
 
 # Parse the environment variables from .env file, ignoring the comments
 export $(cat .env | grep -v ^# | xargs)
