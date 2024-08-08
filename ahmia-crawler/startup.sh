1f8e23ba4fc6fc65b84da7d4ce45e1f0162001d3
Alireza Gharib <alirezagharib110@gmail.com>
2024-08-08 13:08:40 +0330

    Update startup.sh

--- ahmia-crawler/startup.sh
+++ ahmia-crawler/startup.sh
@@ -22,11 +22,9 @@ sleep 3m
 # Run your initial commands
 bash /root/codes/run.sh &> /root/codes/crawler.log
 
-# Set up cron jobs
-cron
 
 # Add a simple line to cron.log
 echo "Cron job setup started." >> /var/log/cron.log
 
 # Tail cron log to keep the container running
-tail -f /var/log/cron.log
+cron && tail -f /var/log/cron.log
